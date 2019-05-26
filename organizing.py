f9404ec4700af42d57db8912fea2d6db59cbe02f
gmanacce <49345640+gmanacce@users.noreply.github.com>
2019-05-26 19:39:48 +0300

    Create organizing.py

---
+++ organizing.py
@@ -0,0 +1,46 @@
+#### 11 / 11 /2019
+#### AUTHOR Geofrey Manacce
+#### Topic ORGANIZING_LIST
+
+
+#####creating any list
+
+birthday_months = ['january','february', 'june','september','november','december']
+
+##### organize by using sort method
+
+birthday_months.sort()
+
+print(birthday_months)
+
+#####using a reverse sort method
+
+birthday_months = ['january','february', 'june','september','november','december']
+
+birthday_months.sort(reverse = True)
+
+print(birthday_months)
+
+######using sorted method to temporary reverse list
+
+birthday_months = ['january','february', 'match', 'april', 'may','june','july','august','september','october','november','december']
+
+print(sorted(birthday_months))
+
+print(birthday_months)
+
+######using reverse method to a list
+
+week_days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+
+print(week_days)
+
+week_days.reverse()
+
+print(week_days)
+
+########how to find the length of the list using len() method
+
+print(len(week_days))
+
+print(len(birthday_months))
