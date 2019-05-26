2a507cbdb4b5d56a92657075f0060e7a40b45dd0
gmanacce <49345640+gmanacce@users.noreply.github.com>
2019-05-26 19:42:11 +0300

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,15 @@
+#### 11 / 05 / 2019
+#### Author GEOFREY MANACCE
+#### TUPLE /value that never change for lifetime o the program
+
+#####Example
+coodinates = (1002 , 5005)
+for coodinate in coodinates:
+	print(coodinate)
+
+#####The only way to modify tuple is to overlide the value
+
+coodinates = (20005 , 76000)
+for coodinate in coodinates:
+	print(coodinate)
+print(coodinates)
