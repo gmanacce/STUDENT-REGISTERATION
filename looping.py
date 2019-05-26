66da17fcee8f801c5a98c9ffda70d50b87c75958
gmanacce <49345640+gmanacce@users.noreply.github.com>
2019-05-26 19:40:42 +0300

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,17 @@
+###### 11 / 05 / 2019
+###### Author Geofrey Manacce
+###### LOOPING PROGRAM
+
+####CREATE A LIST
+months = ['january','february', 'match', 'april', 'may','june','july','august','september','october','november','december']
+print(months)
+#####using For loop
+
+for month in months:
+	print(month.title() + "\n")
+
+	print("go for next month:")
+
+
+print("GOODBYE!!!!")
+
